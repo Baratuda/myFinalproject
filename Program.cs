ea9eecbb61b0d404e4eae2251c896a9c94d4914e
Egor Baranovskiy <egorpifagop@gmail.com>
2023-03-08 22:56:31 +0300

    the solution  of my task was added.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+string[] ourArray = {"hello", "hell", "cat", "as", "at"};
+int arrLength = ourArray.Length; 
+int y = 0;
+string[] arrConsistOfThreeLettersWorld = new string[arrLength];
+
+
+for(int i = 0; i<arrLength; i++)
+   if(ourArray[i].Length<=3) {
+      arrConsistOfThreeLettersWorld[y] = ourArray[i];
+      y++;
+   }
+
+ourArray = new string[y+1];
+for(int i = 0; i<y+1; i++) ourArray[i] = arrConsistOfThreeLettersWorld[i];
+string result = "[ "+string.Join(" ", ourArray)+"]";
+Console.Write(result);
+   
